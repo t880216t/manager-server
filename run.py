0fcf5756dfff600ce6bcc519f878ec1a1770f9be
t880216t <562746248@qq.com>
2017-12-12 09:23:50 +0800

    debug服务器脚本2次执行问题修改

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ def run_listen():
 if __name__ == '__main__':
     t3 = Thread(target=run_listen)
     t3.start()
-    app.run(host='0.0.0.0', debug=True, threaded=True)
+    app.run(host='0.0.0.0', debug=True, threaded=True, use_reloader=False)
